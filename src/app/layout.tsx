6198971db85d76f4e87aec8b0a98aea38368a958
Erkebulan <mecta@MacBook-Pro---Erkebulan.local>
2026-03-04 13:06:03 +0500

    google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const metadata = {
   title: "Kulash Halal Market",
   verification: {
-    google: "hasAPNB0L8Sdk2wg3zqv1LhsTyV9tPo85a2rF0UTm0w",
+    google: "6JuxN9FeXtNswnWD6Tx5kUxzRbRb1Zzheo2MxSxHGUo",
   },
 };
 
